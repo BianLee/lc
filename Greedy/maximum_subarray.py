97f1c7679688b923fa0b722270cf77c212d1efe2
Bian Lee <bianlee2004@gmail.com>
2024-04-01 14:43:24 -0700

    .

---
+++ Greedy/maximum_subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum = nums[0]
+        sum = 0
+        for n in nums:
+            sum += n
+            maxSum = max(maxSum, sum)
+            if sum < 0:
+                sum = 0
+        return maxSum
