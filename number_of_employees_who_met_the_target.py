f6b5223a2173baa235acfd97d79b0b08c4a0d3cb
Bian Lee <bianlee2004@gmail.com>
2024-04-16 17:02:24 -0700

    >

---
+++ number_of_employees_who_met_the_target.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        count=0
+        for i in hours:
+            if target <= i:
+                count+=1
+        return count
