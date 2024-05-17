40d7513016aeb1a65aed600b062c5aedb0e38eac
Bian Lee <62369269+BianLee@users.noreply.github.com>
2024-05-16 21:03:42 -0700

    Update pascals_triangle.py

--- Easy Problems/pascals_triangle.py
+++ Easy Problems/pascals_triangle.py
@@ -1,19 +1,19 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        memo = {}
+        memo = dict()
         def dp(numRows):
             if numRows in memo:
                 return memo[numRows]
             if numRows == 1:
-                memo[numRows] = [[1]]
-                return memo[numRows]
+                return [[1]]
             if numRows == 2:
-                memo[numRows] = [[1], [1, 1]]
-                return memo[numRows]
-            lastArr = dp(numRows - 1)
-            newArr = [1]
-            for i in range(len(lastArr[-1]) - 1):
-                newArr.append(lastArr[-1][i] + lastArr[-1][i + 1])
+                return [[1], [1, 1]]
+            newArr = list()
+            newArr.append(1)
+            lastArr = dp(numRows-1)
+            for i in range(len(lastArr[-1])):
+                if i+1<len(lastArr[-1]):
+                    newArr.append(lastArr[-1][i] + lastArr[-1][i+1])
             newArr.append(1)
             memo[numRows] = lastArr + [newArr]
             return memo[numRows]
