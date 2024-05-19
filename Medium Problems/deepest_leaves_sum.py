f261f734a595bb7d510738e0605182e206b4e719
Bian Lee <62369269+BianLee@users.noreply.github.com>
2024-05-18 17:10:16 -0700

    Create deepest_leaves_sum.py

---
+++ Medium Problems/deepest_leaves_sum.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        
+        q = deque()
+        if root:
+            q.append(root)
+        sum=0
+        while q:
+            level=[]
+            for i in range(len(q)):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            sum=0
+            for n in level:
+                sum+=n
+        return sum
