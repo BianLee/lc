9251ff6ab41a1c914d410f02b66420e90dbf18e0
Bian Lee <62369269+BianLee@users.noreply.github.com>
2024-05-21 16:39:17 -0700

    Update count_good_nodes_in_binary_tree.py

--- Trees/count_good_nodes_in_binary_tree.py
+++ Trees/count_good_nodes_in_binary_tree.py
@@ -15,3 +15,17 @@ class Solution:
             else:
                 return dfs(root.left, maxVal) + dfs(root.right, maxVal)
         return dfs(root, root.val)
+
+
+# Another solution
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(root, maxVal):
+            count=0
+            if not root:
+                return 0
+            if root.val >= maxVal:
+                maxVal=root.val
+                count+=1
+            return dfs(root.left, maxVal) + dfs(root.right, maxVal) + count
+        return dfs(root, root.val)
