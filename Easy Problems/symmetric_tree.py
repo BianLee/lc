8991e87068d875ef2b1b3bb0091af5f2c90b1560
Bian Lee <62369269+BianLee@users.noreply.github.com>
2024-05-22 18:33:56 -0700

    Create symmetric_tree.py

---
+++ Easy Problems/symmetric_tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        final=[]
+        q = deque()
+        if root:
+            q.append(root)
+        while q:
+            level=[]
+            for i in range(len(q)):
+                node=q.popleft()
+                if node:
+                    level.append(node.val)
+                    if node.left:
+                        q.append(node.left)
+                    else:
+                        q.append(None)
+                    if node.right:
+                        q.append(node.right)
+                    else:
+                        q.append(None)
+                else:
+                    level.append("None")
+
+            final.append(level)
+        for i in range(len(final)):
+            if final[i] != final[i][::-1]:
+                return False
+        return True
