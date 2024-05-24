a47a96c3c2c73ba517800ba3794cef66a7857d4c
Bian Lee <62369269+BianLee@users.noreply.github.com>
2024-05-23 23:16:39 -0700

    Update merge_intervals.py

--- Medium Problems/merge_intervals.py
+++ Medium Problems/merge_intervals.py
@@ -12,3 +12,17 @@ class Solution:
             else:
                 output.append([start,end])
         return output
+
+# Another solution
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda pair:pair[0])
+        output=[intervals[0]]
+
+        for i in range(len(intervals)):
+            if output[-1][1] >= intervals[i][0]:
+                output[-1][1] = max(output[-1][1], intervals[i][1])
+            else:
+                output.append(intervals[i])
+        return output
+            
