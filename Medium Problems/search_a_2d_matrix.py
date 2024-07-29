746e610b77aba057db44d4890f8cf45302e5702b
Bian Lee <62369269+BianLee@users.noreply.github.com>
2024-07-29 11:54:08 -0400

    Update search_a_2d_matrix.py

--- Medium Problems/search_a_2d_matrix.py
+++ Medium Problems/search_a_2d_matrix.py
@@ -24,3 +24,32 @@ class Solution:
             else:
                 return True
         return False
+
+
+
+# another solution
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        l_row, r_row = 0, len(matrix)-1
+        l_col, r_col = 0, len(matrix[0])-1
+ 
+        while l_row <= r_row:
+            m_row = (l_row + r_row)//2
+            if matrix[m_row][0] > target:
+                r_row -= 1
+            elif matrix[m_row][0] < target:
+                l_row +=1
+            else:
+                return True
+        print(r_row)
+        while l_col <= r_col:
+            m_col = (l_col + r_col)//2
+            if matrix[r_row][m_col] > target:
+                r_col -= 1
+            elif matrix[r_row][m_col] < target:
+                l_col += 1
+            else:
+                return True
+        
+        return False
