ee7c5f9407a30542f79962a3a70ddaf066d3e2de
Bian Lee <62369269+BianLee@users.noreply.github.com>
2024-08-31 00:34:53 -0700

    Update evaluate_reverse_polish_notation.py

--- Medium Problems/evaluate_reverse_polish_notation.py
+++ Medium Problems/evaluate_reverse_polish_notation.py
@@ -15,3 +15,36 @@ class Solution:
             else:
                 stack.append(int(c))
         return stack.pop()
+
+
+alternative solution
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        
+        stack = []
+        for c in tokens:
+            if c == "+":
+                first = stack.pop()
+                second = stack.pop()
+                stack.append(first + second)
+            
+            elif c == "-":
+                first = stack.pop()
+                second = stack.pop()
+                stack.append(second - first)
+
+            elif c == "*":
+                first = stack.pop()
+                second = stack.pop()
+                stack.append(first * second)
+            
+            elif c == "/":
+                first = stack.pop()
+                second = stack.pop()
+                stack.append(int(second / first))
+
+            else:
+                stack.append(int(c))
+        
+        return stack[-1]
