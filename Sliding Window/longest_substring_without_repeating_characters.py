b7625ec4cfa7a85779aee090851d288d73c2985e
Bian Lee <62369269+BianLee@users.noreply.github.com>
2024-08-30 21:56:23 -0700

    Update longest_substring_without_repeating_characters.py

--- Sliding Window/longest_substring_without_repeating_characters.py
+++ Sliding Window/longest_substring_without_repeating_characters.py
@@ -10,3 +10,22 @@ class Solution:
             charSet.add(s[r])
             longest = max(longest, r-l+1)
         return longest
+
+
+#Different solution
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l,r=0,0
+        charSet = set()
+        maxCount = 0
+        while l<=r<len(s):
+          
+            while r<len(s) and s[r] not in charSet:
+                charSet.add(s[r])
+                maxCount = max(maxCount, len(charSet))
+                r+=1
+            else: #s[r] is in charSet
+                charSet.remove(s[l])
+                l+=1
+
+        return maxCount
