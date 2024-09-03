7fa6cddb370e2d29c465d5595bdde47d39662e25
Bian Lee <62369269+BianLee@users.noreply.github.com>
2024-09-03 13:26:37 -0700

    Update find_minimum_in_rotated_sorted_array.py

--- Binary Search/find_minimum_in_rotated_sorted_array.py
+++ Binary Search/find_minimum_in_rotated_sorted_array.py
@@ -12,4 +12,19 @@ class Solution:
 
 
 
-    
+# one that uses (l+r)//2 to calculate midpoint
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        l, r = 0, len(nums)-1
+        while l<r:
+            m = (l+r)//2
+            #compare mid element with the rightmost element
+
+            # if the mid element is greater than the rightmost
+            if nums[m] > nums[r]: 
+                l = m+1
+            else:
+                r = m
+        return nums[l]
