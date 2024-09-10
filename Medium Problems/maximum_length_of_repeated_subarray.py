6ba804b765f3616167bdf60e31945e98e9490337
Bian Lee <62369269+BianLee@users.noreply.github.com>
2024-09-09 17:09:55 -0700

    Update maximum_length_of_repeated_subarray.py

--- Medium Problems/maximum_length_of_repeated_subarray.py
+++ Medium Problems/maximum_length_of_repeated_subarray.py
@@ -10,3 +10,24 @@ class Solution:
                 output = max(output, dp[i][j])
     
         return output
+
+
+# alternative
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+    
+        dp = [[0 for _ in range(len(nums2))] for _ in range(len(nums1))]
+
+        output = 0
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = dp[i-1][j-1]+1
+                    output = max(output, dp[i][j])
+        
+        #print(dp)
+        
+        return output
