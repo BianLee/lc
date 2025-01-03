b84a06d2183f51a2c85857d95d6dc1fb23fcabe6
Bian Lee <62369269+BianLee@users.noreply.github.com>
2025-01-03 10:53:14 -0800

    Update merge_sorted_array.py

--- Easy Problems/merge_sorted_array.py
+++ Easy Problems/merge_sorted_array.py
@@ -15,3 +15,35 @@ class Solution:
             nums1[last] = nums2[n-1]
             n-=1
             last-=1
+
+#alternative solution
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+        tempOne = []
+        for i in range(m):
+            tempOne.append(nums1[i])
+
+        onePointer, twoPointer = 0,0
+        index = 0
+        while onePointer<m and twoPointer<n:
+            if tempOne[onePointer] < nums2[twoPointer]:
+                nums1[index] = tempOne[onePointer]
+                onePointer +=1
+            else:
+                nums1[index] = nums2[twoPointer]
+                twoPointer+=1
+            index+=1
+        while onePointer<m:
+            nums1[index] = tempOne[onePointer]
+            onePointer+=1
+            index+=1
+
+        while twoPointer<n:
+            nums1[index] = nums2[twoPointer]
+            twoPointer+=1
+            index+=1
+        
