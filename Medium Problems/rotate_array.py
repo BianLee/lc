38a27c0a8d4c2c9eeaff93156baaa37b23bb475f
Bian Lee <62369269+BianLee@users.noreply.github.com>
2025-01-03 12:29:46 -0800

    Update rotate_array.py

--- Medium Problems/rotate_array.py
+++ Medium Problems/rotate_array.py
@@ -4,3 +4,21 @@ class Solution:
         nums.reverse()
         nums[:k] = nums[:k][::-1]
         nums[k:] = nums[k:][::-1]
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+
+        if k>len(nums):
+            k = k % len(nums)
+        memory = []
+        for i in range(len(nums)-k):
+            memory.append(nums[i])
+        index = 0
+        for i in range(len(nums)-k, len(nums)):
+            nums[index] = nums[i]
+            index+=1
+        for i in range(len(memory)):
+            nums[index] = memory[i]
+            index+=1
+
+    
