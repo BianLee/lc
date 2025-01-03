d098b29952699f8a75dadfd13003a5e5ac47ea20
Bian Lee <62369269+BianLee@users.noreply.github.com>
2025-01-03 12:40:07 -0800

    Update best_time_to_buy_and_sell_stock_ii.py

--- Medium Problems/best_time_to_buy_and_sell_stock_ii.py
+++ Medium Problems/best_time_to_buy_and_sell_stock_ii.py
@@ -5,3 +5,18 @@ class Solution:
             if i+1<len(prices) and prices[i+1]-prices[i]>0:
                 count+=prices[i+1]-prices[i]
         return count
+
+#alternative
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        l = 0
+        maxSum = 0
+        
+        for i in range(l+1, len(prices)):
+            if prices[l] > prices[i]:
+                l=i
+            else:
+                maxSum += prices[i]-prices[l]
+                l+=1
+        return maxSum
